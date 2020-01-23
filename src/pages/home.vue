7b79730d0755bdbf78646d6c957a8099ea3d9939
Ricardo <568082704@qq.com>
2020-01-23 22:30:02 +0800

    save

---
+++ src/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
