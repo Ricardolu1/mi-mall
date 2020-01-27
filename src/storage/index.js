9e1056e76ec71e59f016e6dca805289c7d9efd51
Ricardo <568082704@qq.com>
2020-01-27 23:22:50 +0800

    save

---
+++ src/storage/index.js
@@ -0,0 +1,62 @@
+// Storage封装
+
+const STORAGE_KEY = 'mall'
+
+export default {
+  //存储值
+  setItem(key,value,module_name){
+    if (module_name) {
+      let val = this.getItem(module_name)
+      val[key] = value
+      this.setItem(module_name,val)
+    }else{
+      let val = this.getStorage()
+      val[key] = value
+      window.sessionStorage.setItem(STORAGE_KEY,JSON.stringify(val))
+    }
+  },
+  //获取某个某块下面的属性举例 user下面的user下面userName
+  getItem(key, module_name){
+    if (module_name) {
+      let val = this.getItem(module_name)
+      if (val) {
+        return val[key]
+      }
+    }
+    return this.getStorage()[key]
+  },
+  getStorage(){
+     return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY)||'{}')
+  },
+  clear(key,module_name){
+    let val = this.getStorage()
+    if (module_name) {
+      delete val[module_name][key]
+    }else{
+      delete val[key]
+    }
+    this.setItem()
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
