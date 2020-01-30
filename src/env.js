79818faf4dace866f773a19a5b2718cbef900e0c
Ricardo <568082704@qq.com>
2020-01-30 22:34:04 +0800

    环境设置

--- src/env.js
+++ src/env.js
@@ -1,6 +1,23 @@
 let baseURL
+switch (process.env.NODE_ENV) {
+  case 'development':
+    baseURL = 'http://dev-mall-pre.springboot.cn/api'
+    console.log('11')
+    break;
+  case 'test':
+    baseURL = 'http://test-mall-pre.springboot.cn/api'
+    console.log('22')
+    break;
+  case 'prod':
+    baseURL = 'http://mall-pre.springboot.cn/api'
+    console.log('33')
+    break;
+  default:
+    baseURL = 'http://mall-pre.springboot.cn/api'
+    console.log('44')
+    break;
+}
 
-if (env===) {}
 
 export default {
   baseURL
--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import env from './env'
 
 import App from './App.vue'
 
 //根据前端的跨域方式做调整  /a/b : /api/a/b =>/a/b
-axios.defaults.baseURL = '/api'  //跟webpack配置联系起来
+// axios.defaults.baseURL = '/api'  //跟webpack配置联系起来
 axios.defaults.timeout = 8000
-
+//根据环境变量获取不同的环境地址
+axios.defaults.baseURL = env.baseURL
 //接口错误拦截
 axios.interceptors.response.use(function (response) {
   let res = response.data
